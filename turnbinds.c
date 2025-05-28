c04672ff94ada8c012f2b549df97da98f63eac6a
lucy <36128102+lucyksf@users.noreply.github.com>
2025-05-28 20:45:18 +0200

    Create turnbinds.c

---
+++ turnbinds.c
@@ -0,0 +1,131 @@
+#include <fcntl.h>
+#include <linux/input.h>
+#include <linux/uinput.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <poll.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define die(str, args...) do { perror(str); exit(EXIT_FAILURE); } while(0)
+
+// settings!
+const int left_speed = 150;
+const int right_speed = 150;
+const int override_speed = 600;
+
+const int override_key_code = KEY_E;        // press and hold to use the override speed
+const int toggle_key_code = KEY_COMMA;      // press to toggle turnbinds on/off
+
+
+void emit(int fd, int type, int code, int val) {
+    struct input_event ie;
+
+    ie.type = type;
+    ie.code = code;
+    ie.value = val;
+    gettimeofday(&ie.time, NULL);
+
+    if(write(fd, &ie, sizeof(ie)) < 0) perror("write uinput");
+}
+
+int read_mouse_buttons(int fd, bool *left_down, bool *right_down) {
+    struct input_event ev;
+
+    while(read(fd,&ev,sizeof(ev)) == sizeof(ev)) {
+        if(ev.type == EV_KEY) {
+            if(ev.code == BTN_LEFT) *left_down = (ev.value != 0);
+            else if(ev.code == BTN_RIGHT) *right_down = (ev.value != 0);
+        }
+    }
+    return 0;
+}
+
+int main(void) {
+    int ufd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    if(ufd < 0) die("open uinput");
+
+    ioctl(ufd, UI_SET_EVBIT, EV_KEY);
+    ioctl(ufd, UI_SET_KEYBIT, BTN_LEFT);
+    ioctl(ufd, UI_SET_KEYBIT, BTN_RIGHT);
+    ioctl(ufd, UI_SET_EVBIT, EV_REL);
+    ioctl(ufd, UI_SET_RELBIT, REL_X);
+    ioctl(ufd, UI_SET_RELBIT, REL_Y);
+    ioctl(ufd, UI_SET_EVBIT, EV_SYN);
+
+    struct uinput_setup usetup = {0};
+    snprintf(usetup.name, UINPUT_MAX_NAME_SIZE, "virtual-mouse");
+    usetup.id.bustype = BUS_USB;
+    usetup.id.vendor  = 0x1234;
+    usetup.id.product = 0x5678;
+    usetup.id.version = 1;
+
+    ioctl(ufd, UI_DEV_SETUP, &usetup);
+    ioctl(ufd, UI_DEV_CREATE);
+    sleep(1);
+
+    // adjust these to your actual input devices (dev/input/eventX, X=YOUR DEVICE)
+    // to find the directory of your inputs, install "evtest" (same name on Debian based distribution & on Arch!), then run "evtest" in your terminal
+    // select "xyz keyboard" and "xyz mouse" and find the correct one
+    int kb_fd = open("/dev/input/event6", O_RDONLY | O_NONBLOCK);       // Keyboard
+    int mouse_fd = open("/dev/input/event3", O_RDONLY | O_NONBLOCK);    // Mouse
+
+    if(kb_fd < 0 || mouse_fd < 0) die("open input devices");
+
+    struct pollfd fds[2] = {
+        { .fd = kb_fd, .events = POLLIN },
+        { .fd = mouse_fd, .events = POLLIN }
+    };
+
+    bool left_down = false, right_down = false;
+    bool override_down = false;
+    bool turnbinds_enabled = true;
+
+    printf("Turnbinds initially ENABLED. Press ',' (comma) to toggle.\n");
+
+    while (1) {
+        if(poll(fds, 2, 10) < 0) die("poll");
+
+        // keyboard input
+        if(fds[0].revents & POLLIN) {
+            struct input_event ev;
+            
+            while(read(kb_fd, &ev, sizeof(ev)) == sizeof(ev)) {
+                if(ev.type == EV_KEY) {
+                    if(ev.code == override_key_code) override_down = (ev.value != 0);
+                    else if(ev.code == toggle_key_code && ev.value == 1) { // toggle key pressed
+                        turnbinds_enabled = !turnbinds_enabled;
+                        system("clear");
+                        printf("Turnbinds %s\n", turnbinds_enabled ? "ENABLED" : "DISABLED");
+                    }
+                }
+            }
+        }
+
+        // mouse buttons
+        read_mouse_buttons(mouse_fd, &left_down, &right_down);
+
+        if(turnbinds_enabled) {
+            int ls = override_down ? override_speed : left_speed;
+            int rs = override_down ? override_speed : right_speed;
+
+            if(left_down && !right_down) {
+                emit(ufd, EV_REL, REL_X, -ls);
+                emit(ufd, EV_SYN, SYN_REPORT, 0);
+            } else if(right_down && !left_down) {
+                emit(ufd, EV_REL, REL_X, rs);
+                emit(ufd, EV_SYN, SYN_REPORT, 0);
+            }
+        }
+
+        usleep(9500); //usleep(10000);
+        // feel free to mess around with the timing,
+        // it changes the smoothness and sensitvitiy (requires look_left, look_right & override_speed to be changed as well!!)
+    }
+
+    ioctl(ufd, UI_DEV_DESTROY);
+    close(ufd);
+    return 0;
+}
